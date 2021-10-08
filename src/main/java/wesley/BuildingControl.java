c29a967011245c4236be80ed6a3bfbba8fd06775
wesleylam <wesleylam1b@gmail.com>
2021-10-08 15:45:44 +1100

    maven init

---
+++ src/main/java/wesley/BuildingControl.java
@@ -0,0 +1,8 @@
+package wesley;
+
+public class BuildingControl{
+    // program entry point
+    public static void main(String[] args) {
+        System.out.println("Output");
+    }
+}
---
+++ src/test/java/wesley/BuildingControlTest.java
@@ -0,0 +1,20 @@
+package wesley;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BuildingControlTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
